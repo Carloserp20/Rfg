f14ae5a84c5171aabb929984b7f1f816e4ddda0e
Carloserp20 <161874064+Carloserp20@users.noreply.github.com>
2024-03-03 09:56:14 -0600

    primer commet 2

---
+++ rfc.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Carlos Arturo
+ */
+public class rfc {
+    public rfc(){
+        
+    }
+    Integer dia=0, año=0, mes=0;
+    public void getFecha(String a, String b, String c){
+        
+        dia=Integer.valueOf(a);
+        mes=Integer.parseInt(b);
+        año=Integer.parseInt(c);
+    }
+}
